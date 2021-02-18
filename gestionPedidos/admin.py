2e2200268f66d0140c0d8a60d41a0b467399bf1c
Crhistian Mendoza <crhisclick@gmail.com>
2021-02-18 17:09:00 -0400

    agregado clase articulos y pedios en el panel de administracion con list_display, en pedidos adicionalmente se agrego list_filter por fecha y date_hierarchy

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -7,8 +7,15 @@ from gestionPedidos.models import Cliente, articulos, pedidos
 class ClienteAdmin(admin.ModelAdmin):
     list_display=("nombre","direccion","telefono")
     search_fields=("nombre","telefono")
+class ArticulosAdmin(admin.ModelAdmin):
+    list_display=('nombre','seccion','precio')
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=('numero','fecha')
+    list_filter=('fecha',)
+    date_hierarchy='fecha'
 
 
 admin.site.register(Cliente,ClienteAdmin)
-admin.site.register(articulos)
-admin.site.register(pedidos)
+admin.site.register(articulos, ArticulosAdmin)
+admin.site.register(pedidos, PedidosAdmin)
