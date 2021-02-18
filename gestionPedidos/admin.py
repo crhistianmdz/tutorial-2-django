41d8586411592c87429bc7907144d7a949a565d2
Crhistian Mendoza <crhisclick@gmail.com>
2021-02-18 14:51:05 -0400

    agregado nombres para mostrar en el modelo cliente, agregado verbose name para direccion de cliente, se agrego modelos al admin.py para modificar desde el panel de administracion, lista de nombres modificados para mostrar y creada casilla de busqueda

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from gestionPedidos.models import Cliente, articulos, pedidos
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display=("nombre","direccion","telefono")
+    search_fields=("nombre","telefono")
+
+
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(articulos)
+admin.site.register(pedidos)
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -4,9 +4,13 @@ from django.db import models
 
 class Cliente(models.Model):
     nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    direccion=models.CharField(max_length=50,verbose_name="la direccion")
+    email=models.EmailField(blank=True, null=True)
     telefono=models.CharField(max_length=7)
+    def __str__(self):
+        return self.nombre
+
+
 
 class articulos(models.Model):
     nombre=models.CharField(max_length=30)
