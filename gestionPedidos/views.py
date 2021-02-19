368fffecd50cde3f76d13b7001283a6e01a370e4
Crhistian Mendoza <crhisclick@gmail.com>
2021-02-19 09:19:36 -0500

    creado formulario de contacto con sus respecticvas rutas y envio de correo, se creo el controlador para enviar correo y se configuro gmail como proveedor de correo mas no se tuvo exito, gmail rechaza el envio de correo a pesar de que la configuracion es la correcta

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -4,6 +4,8 @@ from django.http import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionPedidos.models import articulos
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 
@@ -14,12 +16,22 @@ def buscar(request):
     if request.GET['prd']:
         #mensaje="articulos buscados: %r" %request.GET["prd"]
         producto=request.GET['prd']
-        articulo=articulos.objects.filter(nombre__icontains=producto)
-        return render(request, 'resultado_busqueda.html',{'articulos':articulo,'query':producto})
+        if len(producto)>20:
+            mensaje='texto demasiado largo'
+        else:
+            articulo=articulos.objects.filter(nombre__icontains=producto)
+            return render(request, 'resultado_busqueda.html',{'articulos':articulo,'query':producto})
     else:
         mensaje='no ha introducido nada'
-
-
-
-    
     return HttpResponse(mensaje)
+
+def contacto(request):
+
+    if request.method=="POST":
+        subject=request.POST["asunto"]
+        message=request.POST['mensaje']+" "+ request.POST["email"]
+        email_from=settings.EMAIL_HOST_USER
+        recipient_list=["crhistianmendoza5@gmail.com"]
+        send_mail(subject, message,email_from,recipient_list)
+        return render(request,'gracias.html')
+    return render(request,'contacto.html')
--- tiendaOnline/settings.py
+++ tiendaOnline/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_USE_TLS=True
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+
+EMAIL_PORT=465#587
+EMAIL_HOST_USER="crhistianmendoza5@gmail.com"
+EMAIL_HOST_PASSWORD="Engel_1807"
